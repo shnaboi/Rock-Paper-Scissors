f272ce236f0e92b4a77b4b94a4c64ed66c72d0f9
shnaboi <shane.karner@gmail.com>
2023-01-10 01:07:41 -0500

    Add input from user via window prompt

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // document.addEventListener('DOMContentLoaded', () => {
 
+    let player;
+
     function getComputerChoice() {
         let number = Math.floor(Math.random() * 3 + 1);
         if (number == 1) {
@@ -13,8 +15,25 @@
 
     function playRound(player, computer) {
         
+        getComputerChoice();
+
     }
 
     function game() {
 
+        playRound();
     }
+
+    function getPlayerChoice() {
+        let input = prompt('Rock, Paper, Scissors. Type in "R", "P", or "S" to play');
+        if (input.toLowerCase() == "r") {
+            player = "rock";
+        } if (input.toLowerCase() == "p") {
+            player = "paper";
+        } else if (input.toLowerCase() == "s") {
+            player = "scissors"
+        }
+        console.log(player)
+    }
+
+    getPlayerChoice();
