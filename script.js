d1e3b4941d7aa99f31ec7746836847c0165d4101
shnaboi <shane.karner@gmail.com>
2023-01-10 00:45:39 -0500

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+    function getComputerChoice() {
+        let number = Math.floor(Math.random() * 3 + 1);
+        if (number == 1) {
+            return 'rock';
+        } if (number == 2) {
+            return 'paper'
+        } else if (number == 3) {
+            return 'scissors'
+        }
+    }
+
+    function playRound(player, computer) {
+        
+    }
+
+    function game() {
+
+    }
